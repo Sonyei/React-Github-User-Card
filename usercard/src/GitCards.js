88dae9ee62c2971847a061a2e7baf6d07f45479d
Sonyei <esonyei@gmail.com>
2020-12-01 21:39:27 -0500

    teeniest bit of styling

--- usercard/src/GitCards.js
+++ usercard/src/GitCards.js
@@ -3,11 +3,11 @@ import React from 'react'
 function GitCards(props) {
 
     return(
-        <>
+        <div>
           <h1>{props.user.login}</h1>
-            <img width={"60"} alt={"Profile Avatar"} src={props.user.avatar_url}/>
+            <img className="pics" width={"60"} alt={"Profile Avatar"} src={props.user.avatar_url}/>
             <h2>Currently Following {props.user.login}</h2>
-        </>
+        </div>
     )
 }
 
